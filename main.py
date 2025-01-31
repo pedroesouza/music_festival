ed4524eeb5c08fdf897d8f9861b3da02b318c76b
pedroesouza <pedro.souza@ucas-edu.net>
2025-01-31 20:44:03 +0000

    ouiasdksadf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-artistDict = {"example": ["Name", "Genre", "Duration"]}
+artistDict = {"example": ["examp", "Genre", "Duration"]}
 
 print(r''' █████╗ ██████╗ ████████╗██╗███████╗████████╗    ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗███╗   ███╗███████╗███╗   ██╗████████╗
 ██╔══██╗██╔══██╗╚══██╔══╝██║██╔════╝╚══██╔══╝    ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝████╗ ████║██╔════╝████╗  ██║╚══██╔══╝
@@ -8,7 +8,23 @@ print(r''' █████╗ ██████╗ ████████╗�
 ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝╚══════╝   ╚═╝       ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   
                                                                                                                                              ''')
 
-def add_artist():
+def add_artist(artistName, artistGenre, performaceDuration):
+    artistDict[artistName] = [artistName, artistGenre, performaceDuration]
+
+def remove_artist(artistName):
+    del artistDict[artistName]
+
+def edit_artist(artistName, whatToEdit, change):
+    if whatToEdit == "name":
+        artistDict[artistName][0] = change
+        artistDict[change] = artistDict.pop(artistName)
+    
+        
+    if whatToEdit == "genre":
+        artistDict[artistName][1] = change
+
+
+def search_artist(artistName):
     pass
 
 def artist_management():
@@ -18,11 +34,19 @@ def artist_management():
   |_|_|   _|_|_   _\__|   _|_|_   /__/_   _\__|   TS__[O]|_|__|_| \__,_|  |_||_|  \__,_|   |___/   \___|  |_|_|_|  \___|  |_||_|   _\__|  
 _|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| {======|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| 
 "`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'./o--000'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' ''')
+    
+    print(artistDict)
 
 
-    artistListInteraction = input("\nWould you like to add, edit or remove artist and info? ").lower()
+    artistListInteraction = input("\nWould you like to add, edit, remove artist and info or search? ").lower()
     if artistListInteraction == "add":
         add_artist(input("What is the artists name? "), input("What is the artists genre? "), input("What is the duration of the performance? "))
+    if artistListInteraction == "remove":
+        remove_artist(input("What artist would you like to remove? "))
+    if artistListInteraction == "edit":
+        edit_artist(input("What artist do you want to edit? "), input("Would you like to edit the name, genre, or duration? ").lower(), input("What do you want to change it to? "))
+    if artistListInteraction == "search":
+        search_artist(input("What is your artist's name? "))
 
-
-artist_management()
+while True:
+    artist_management()
